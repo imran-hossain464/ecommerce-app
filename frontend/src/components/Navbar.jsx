106e9f90794864cc37345c3fc58b087e6e106666
Md. Imran Hossain <k.imran.khan464@gmail.com>
2025-03-26 00:37:37 +0600

    Version 2

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { NavLink } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [visible, setVisible] = useState(false);
+
+
+
+
+
+
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <img src={assets.logo} alt="logo" className="w-36" />
@@ -49,14 +55,32 @@ const Navbar = () => {
             </div>
           </div>
         </div>
-        <Link to="/cart" className="relative"> 
-        <img
-          src={assets.cart_icon}
-          alt="cart icon"
-          className="w-5 min-w-5"/>
-          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">10</p>
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} alt="cart icon" className="w-5 min-w-5" />
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+            10
+          </p>
         </Link>
-        <img src={assets.menu_icon} alt="menu icon" className="w-5 cursor-pointer sm:hidden" />
+        <img onClick={() => setVisible(true)}
+          src={assets.menu_icon}
+          alt="menu icon"
+          className="w-5 cursor-pointer sm:hidden"
+        />
+      </div>
+      {/*Sidebar Menu for small screen */}
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full': 'w-0'}`}>
+        <div className='flex flex-col text-gray-600'>
+          <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+            <img className='h-4 rotate-180' src={assets.dropdown_icon} alt='dropdown icon'/>
+            <p>Back</p>
+          </div>
+          <NavLink  onClick={()=>(setVisible(false))} className='PY-2  pl-6 border ' to="/" >Home </NavLink>
+          <NavLink onClick={()=>(setVisible(false))} className='PY-2  pl-6 border ' to="/collection" >Collection </NavLink>  
+          <NavLink onClick={()=>(setVisible(false))} className='PY-2  pl-6 border ' to="/about" >About </NavLink>
+          <NavLink  onClick={()=>(setVisible(false))} className='PY-2 pl-6 border 'to="/contact" >Contact </NavLink>
+
+
+        </div>
       </div>
     </div>
   );
