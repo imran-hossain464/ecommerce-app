5fdca08c333bf42eef1e1d08cdfc1a2733cb13dd
Md. Imran Hossain <k.imran.khan464@gmail.com>
2025-03-28 00:57:00 +0600

    Version 5

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
 import { assets } from "../assets/assets";
 
 
 const Footer = () => {
   return (
+    <div>
     <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm '>
 
 <div>
@@ -20,6 +21,20 @@ const Footer = () => {
         <li>Privacy Policy</li>
     </ul>
 </div>
+<div>
+  <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+  <ul className='flex flex-col gap-1 text-gray-600'>
+    <li>+880-1623297921</li>
+    <li>contact@gmail.com</li>
+
+  </ul>
+</div>
+</div>
+<div>
+  <hr/>
+  <p className='py-5 text-sm text-center'>
+ Copyright 2025@ domain.com - All Rights Reserved.</p>
+  </div>
     </div>
   )
 }
